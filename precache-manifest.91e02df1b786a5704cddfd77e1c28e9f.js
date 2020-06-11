0dc28c1410ac03195325901fcc84cf63ed967836
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-06-11 13:57:45 +0530

    Updates

---
+++ precache-manifest.91e02df1b786a5704cddfd77e1c28e9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ae7b1360c3b299e608894b7989bcdb",
+    "url": "/tracker4/index.html"
+  },
+  {
+    "revision": "1ba0917ffed068e5dbda",
+    "url": "/tracker4/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9ba431ddcdde728966bb",
+    "url": "/tracker4/static/css/main.7e18e27f.chunk.css"
+  },
+  {
+    "revision": "1ba0917ffed068e5dbda",
+    "url": "/tracker4/static/js/2.eb92d110.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "/tracker4/static/js/2.eb92d110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba431ddcdde728966bb",
+    "url": "/tracker4/static/js/main.83146eb2.chunk.js"
+  },
+  {
+    "revision": "26947e622f7658cb9894",
+    "url": "/tracker4/static/js/runtime-main.73599399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tracker4/static/media/logo.5d5d9eef.svg"
+  }
+]);
