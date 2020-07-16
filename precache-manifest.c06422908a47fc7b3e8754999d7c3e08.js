6fc93cde6f627810887910a294d7f194647a4b44
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-07-16 23:27:13 +0530

    Updates

---
+++ precache-manifest.c06422908a47fc7b3e8754999d7c3e08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2951c94a2d962947dbe9ffafed4038d",
+    "url": "/tracker4/index.html"
+  },
+  {
+    "revision": "d70edbd5134dfee92a7d",
+    "url": "/tracker4/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "235a5ea229b2cfafecc9",
+    "url": "/tracker4/static/css/main.92e39a76.chunk.css"
+  },
+  {
+    "revision": "d70edbd5134dfee92a7d",
+    "url": "/tracker4/static/js/2.6eb4ab82.chunk.js"
+  },
+  {
+    "revision": "79f10740ecf1353496f83dce558cd480",
+    "url": "/tracker4/static/js/2.6eb4ab82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235a5ea229b2cfafecc9",
+    "url": "/tracker4/static/js/main.478b1093.chunk.js"
+  },
+  {
+    "revision": "26947e622f7658cb9894",
+    "url": "/tracker4/static/js/runtime-main.73599399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tracker4/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tools/ovulation-tracker/precache-manifest.aab5aa772e78ee0c4008a010129f78ee.js"
+  "/tracker4/precache-manifest.c06422908a47fc7b3e8754999d7c3e08.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tools/ovulation-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tracker4/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
